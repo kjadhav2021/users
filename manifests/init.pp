03e38a44dde72c3431785d23aa7faba67a4c36f1
kjadhav2021 <kjadhav102015@gmail.com>
2021-02-17 20:16:34 +0800

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include users
+class users {
+  lookup('users',Hash,'hash').each | String $username, Hash $attrs | {
+    user{ $username:
+      ensure   => 'present',
+      comment  => $attrs['comment'],
+      groups   => $attrs['groups'],
+      password => $attrs['password'],
+    }
+  }
+  lookup('groups',Hash,'hash').each | String $groupname, Hash $grpattrs | {
+    group{ $groupname:
+    ensure  =>'present',
+    members => $grpattrs['members'],
+    }
+  }
+}
