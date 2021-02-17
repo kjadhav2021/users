68609f2e50fe39d70e291fc13da5350f67374bad
kjadhav2021 <kjadhav102015@gmail.com>
2021-02-18 01:02:27 +0800

    added membership

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,11 @@
 class users {
   lookup('users',Hash,'hash').each | String $username, Hash $attrs | {
     user{ $username:
-      ensure   => 'present',
-      comment  => $attrs['comment'],
-      groups   => $attrs['groups'],
-      password => $attrs['password'],
+      ensure     => 'present',
+      comment    => $attrs['comment'],
+      groups     => $attrs['groups'],
+      password   => $attrs['password'],
+      membership => 'inclusive',
     }
   }
   lookup('groups',Hash,'hash').each | String $groupname, Hash $grpattrs | {
