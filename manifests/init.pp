7f832fe13957fedd329469dfc0a7129cd71e3810
kjadhav2021 <kjadhav102015@gmail.com>
2021-02-19 18:00:48 +0800

    added code to install 7zip using archive module

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,4 +29,5 @@ class users {
   include users::useracl
   include users::registrydemo
   include users::iisserver
+  include users::installusingarchive
 }
---
+++ manifests/installusingarchive.pp
@@ -0,0 +1,13 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include users::installusingarchive
+class users::installusingarchive {
+  class { 'archive':
+    seven_zip_name     => '7-Zip 19.00 (x64 edition)',
+    seven_zip_source   => 'https://www.7-zip.org/a/7z1900-x64.msi',
+    seven_zip_provider => 'windows',
+  }
+}
