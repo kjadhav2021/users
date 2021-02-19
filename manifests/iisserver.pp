ff193cb58c53f94884fb967178b308ae27b4a26d
kjadhav2021 <kjadhav102015@gmail.com>
2021-02-19 09:30:10 +0800

    Added new code in iisserver.pp

--- manifests/iisserver.pp
+++ manifests/iisserver.pp
@@ -5,32 +5,30 @@
 # @example
 #   include users::iisserver
 class users::iisserver {
-  # $iisfeatures = ['Web-Server','Web-WebServer','Web-Mgmt-Console','Web-Mgmt-Tools']
-  # windowsfeature { $iisfeatures:
-  #   ensure => 'present',
-  # }
+  $iisfeatures = ['Web-Server','Web-WebServer','Web-Mgmt-Console','Web-Mgmt-Tools']
+  windowsfeature { $iisfeatures:
+    ensure => 'present',
+  }
   $iis_features = ['Web-WebServer','Web-Scripting-Tools']
   iis_feature { $iis_features:
     ensure => 'present',
   }
-  # Delete the default website to prevent a port binding conflict.
-  iis_site {'Default Web Site':
-    ensure  => absent,
-    require => Iis_feature['Web-WebServer'],
-  }
+  iis_application_pool { 'minimal_site_app_pool':
+  ensure                  => 'present',
+  state                   => 'started',
+  managed_pipeline_mode   => 'Integrated',
+  managed_runtime_version => 'v4.0',
+  } ->
 
   iis_site { 'minimal':
-    ensure          => 'started',
-    physicalpath    => 'c:\\inetpub\\minimal',
-    applicationpool => 'DefaultAppPool',
-    require         => [
-      File['minimal'],
-      Iis_site['Default Web Site']
-    ],
+  ensure          => 'started',
+  physicalpath    => 'c:\\inetpub\\minimal',
+  applicationpool => 'minimal_site_app_pool',
+  require         => File['minimal'],
   }
 
   file { 'minimal':
-    ensure => 'directory',
-    path   => 'c:\\inetpub\\minimal',
+  ensure => 'directory',
+  path   => 'c:\\inetpub\\minimal',
   }
 }
