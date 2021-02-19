4902ae3b5076207c3074c36f686c4afff9cf23eb
kjadhav2021 <kjadhav102015@gmail.com>
2021-02-19 12:30:28 +0800

    Added code for default website

--- manifests/iisserver.pp
+++ manifests/iisserver.pp
@@ -5,12 +5,27 @@
 # @example
 #   include users::iisserver
 class users::iisserver {
-  $iis_features = ['Web-Server','Web-WebServer','Web-Mgmt-Console','Web-Mgmt-Tools']
-  windowsfeature { $iis_features:
+  $iisfeatures = ['Web-Server','Web-WebServer','Web-Mgmt-Console','Web-Mgmt-Tools']
+  windowsfeature { $iisfeatures:
     ensure => 'present',
   }
-  # $iis_features = ['Web-WebServer','Web-Scripting-Tools']
-  # iis_feature { $iis_features:
-  #   ensure => 'present',
-  # }
+  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
+  iis_feature { $iis_features:
+    ensure => 'present',
+  }
+  iis_application_pool { 'minimal_site_app_pool':
+    ensure                  => 'present',
+    state                   => 'started',
+    managed_pipeline_mode   => 'Integrated',
+    managed_runtime_version => 'v4.0',
+  } -> iis_site { 'minimal':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\minimal',
+    applicationpool => 'minimal_site_app_pool',
+    require         => File['minimal'],
+  }
+  file { 'minimal':
+    ensure => 'directory',
+    path   => 'c:\\inetpub\\minimal',
+  }
 }
