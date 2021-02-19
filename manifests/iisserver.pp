bfe88a572570b33e910af393458a4debefcc36f3
kjadhav2021 <kjadhav102015@gmail.com>
2021-02-19 09:40:12 +0800

    removed arrow

--- manifests/iisserver.pp
+++ manifests/iisserver.pp
@@ -18,15 +18,13 @@ class users::iisserver {
   state                   => 'started',
   managed_pipeline_mode   => 'Integrated',
   managed_runtime_version => 'v4.0',
-  } ->
-
+  }
   iis_site { 'minimal':
   ensure          => 'started',
   physicalpath    => 'c:\\inetpub\\minimal',
   applicationpool => 'minimal_site_app_pool',
   require         => File['minimal'],
   }
-
   file { 'minimal':
   ensure => 'directory',
   path   => 'c:\\inetpub\\minimal',
