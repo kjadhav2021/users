13e521374126f69fd558e2e7093b46a5bf084dd5
kjadhav2021 <kjadhav102015@gmail.com>
2021-02-19 08:54:18 +0800

    commented window features

--- manifests/iisserver.pp
+++ manifests/iisserver.pp
@@ -5,10 +5,10 @@
 # @example
 #   include users::iisserver
 class users::iisserver {
-  $iisfeatures = ['Web-Server','Web-WebServer','Web-Mgmt-Console','Web-Mgmt-Tools']
-  windowsfeature { $iisfeatures:
-    ensure => 'present',
-  }
+  # $iisfeatures = ['Web-Server','Web-WebServer','Web-Mgmt-Console','Web-Mgmt-Tools']
+  # windowsfeature { $iisfeatures:
+  #   ensure => 'present',
+  # }
   $iis_features = ['Web-WebServer','Web-Scripting-Tools']
   iis_feature { $iis_features:
     ensure => 'present',
