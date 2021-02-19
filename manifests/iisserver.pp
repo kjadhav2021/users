23bca0e227a6341a4f5e0bd6e8a68bf32ad91680
kjadhav2021 <kjadhav102015@gmail.com>
2021-02-19 08:43:46 +0800

    added code for basic website

--- manifests/iisserver.pp
+++ manifests/iisserver.pp
@@ -5,8 +5,33 @@
 # @example
 #   include users::iisserver
 class users::iisserver {
-  $iis_features = ['Web-Server','Web-WebServer','Web-Mgmt-Console','Web-Mgmt-Tools']
-  windowsfeature { $iis_features:
+  $iisfeatures = ['Web-Server','Web-WebServer','Web-Mgmt-Console','Web-Mgmt-Tools']
+  windowsfeature { $iisfeatures:
     ensure => present,
+  }
+  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
+  iis_feature { $iis_features:
+    ensure => 'present',
+  }
+
+  # Delete the default website to prevent a port binding conflict.
+  iis_site {'Default Web Site':
+    ensure  => absent,
+    require => Iis_feature['Web-WebServer'],
+  }
+
+  iis_site { 'minimal':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\minimal',
+    applicationpool => 'DefaultAppPool',
+    require         => [
+      File['minimal'],
+      Iis_site['Default Web Site']
+    ],
+  }
+
+  file { 'minimal':
+    ensure => 'directory',
+    path   => 'c:\\inetpub\\minimal',
   }
 }
